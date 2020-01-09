5669b06fa7f8c5cf97591f8040cdbc5ab01d0f8b
Ayush James <ayush.james1996@gmail.com>
2020-01-09 16:39:19 +1100

    Updates

---
+++ precache-manifest.e0e484b73d7a4931a535554a496ab7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3693c07f7a2d8f41c8b5be7795f4bdd",
+    "url": "/fire-dash/index.html"
+  },
+  {
+    "revision": "ec630fe2a9d421d7e7cb",
+    "url": "/fire-dash/static/css/main.e2b319b1.chunk.css"
+  },
+  {
+    "revision": "22618d958ca6dc8d7882",
+    "url": "/fire-dash/static/js/2.535c6328.chunk.js"
+  },
+  {
+    "revision": "ec630fe2a9d421d7e7cb",
+    "url": "/fire-dash/static/js/main.f14a4d6d.chunk.js"
+  },
+  {
+    "revision": "c023638bed4f475d4045",
+    "url": "/fire-dash/static/js/runtime-main.1e304570.js"
+  },
+  {
+    "revision": "ea7cc9d7d38afdcbc84f1a4e1eead22b",
+    "url": "/fire-dash/static/media/airrow.ea7cc9d7.png"
+  },
+  {
+    "revision": "917141374436665b9de3849961654c06",
+    "url": "/fire-dash/static/media/firechart.91714137.png"
+  }
+]);
